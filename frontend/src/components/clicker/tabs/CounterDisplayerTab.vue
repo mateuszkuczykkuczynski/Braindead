c80737ebf32e688e115054e4d6d2adde037e1ea1
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2025-02-01 20:44:32 +0100

    CORE: Frontend setup

---
+++ frontend/src/components/clicker/tabs/CounterDisplayerTab.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useCounterStore } from "@/stores/counterStore.ts";
+
+const counter = useCounterStore()
+const count = computed(()  => counter.count)
+</script>
+
+
+<template>
+  <h4>{{ count }}</h4>
+</template>
---
+++ frontend/src/views/CounterView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ClickerTab from "@/components/clicker/tabs/ClickerTab.vue";
+</script>
+
+<template>
+  <main>
+    <ClickerTab/>
+  </main>
+</template>
